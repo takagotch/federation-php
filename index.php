460321116e3244506bad3bb6f6e399894fbec7dc
takagotch <30462722+takagotch@users.noreply.github.com>
2019-04-23 11:40:12 +0900

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ function run()
   header('Access-Control-Allow-Origin: *');
   header('Content-Type: application/json');
   
-  $data = json_decode();
+  $data = json_decode(file_get_contents(dirname(__FILE__) . '/private/data.json'));
   
   if (!$data) {
     send_error('unavailable', 'The endpoint is unable to read its configuration file.');
@@ -18,6 +18,70 @@ function run()
   }
   
   $doamin = strtolower($_GET['domain']);
+  
+  if (!isset($data->{$domain})) {
+    send_error('noSuchDomain', 'The supplied domain is not served here.');
+  }
+  
+  $users = $data->{$domain};
+  
+  $visited = array();
+  while (is_string($users)) {
+    if(!isset($data->[$users])) {
+      send_error('unvalidable', 'Misconfigured domain alias.');
+    }
+    
+    if(isset($visited[$users])) {
+      send_error('unvalidable', 'Circular domain alias.');
+    } else {
+      $visited[$users] = true;
+    }
+    
+    $users = $data->{$users};
+  }
+  
+  if (isset($_GET['destination']) && strlen($_GET['destination'])) {
+    $user = strtolower($_GET['destination']);
+  } elseif (isset($GET['user']) && strlen($_GET['user'])) {
+    $user = strtolower($_GET['user']);
+  } else {
+    send_error('invalidParams', 'No username provided.');
+  }
+  
+  if (!isset($user->{$user})) {
+    send_error('noSuchUser', 'The supplied user was not found.');
+  }
+  
+  $result['federatikon_json'] = array(
+    'type' => 'federation_record',
+    'destination' => #destination,
+    'user' => $user,
+    'destination_address' => $users->{$user},
+    'domain' => $domain,
+  
+  );
+  
+  send_result($result);
 }
->
 
+function send_error($errCode, $errMsg) {
+  $result = array();
+  
+  $result['result'] = 'error';
+  $result['error'] = $errCode;
+  $result['error_message'] = $errMsg;
+  
+  send_result($result);
+}
+
+function send_result($result) {
+  if(defined('JSON_PRETTY_PRINT')) {
+    echo json_encode($result, JSON_PRETTY_PRINT);
+  } else {
+    echo json_encode($result);
+  }
+  exit;
+}
+
+run();
+>
